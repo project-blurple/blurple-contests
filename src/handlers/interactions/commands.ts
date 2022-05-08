98992386af949728b2d5e80c5010f1480ef78da6
Glenn <glenn@not-tech.support>
2022-05-08 12:42:38 +0200

    Fix command logging if not found or failed

--- src/handlers/interactions/commands.ts
+++ src/handlers/interactions/commands.ts
@@ -7,13 +7,12 @@ export default async (interaction: CommandInteraction): Promise<void> => {
   const command = interaction.guild?.commands.cache.find(cmd => cmd.name === interaction.commandName);
 
   if (command) {
+    const path = [command.name, interaction.options.getSubcommandGroup(false), interaction.options.getSubcommand(false)].filter(Boolean) as string[];
     try {
-      const path = [command.name, interaction.options.getSubcommandGroup(false), interaction.options.getSubcommand(false)].filter(Boolean).join("/");
-
-      const { default: commandFile } = await import(`../../commands/slash/${path}`) as { default: SlashCommand };
+      const { default: commandFile } = await import(`../../commands/slash/${path.join("/")}`) as { default: SlashCommand };
       void commandFile.execute(interaction);
     } catch (err) {
-      mainLogger.warn(`Command ${interaction.commandName} failed: ${inspect(err)}`);
+      mainLogger.warn(`Command "/${path.join(" ")} failed: ${inspect(err)}`);
     }
   } else mainLogger.warn(`Command ${interaction.commandName} not found.`);
 };
